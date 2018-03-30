3ca35bc505428a5476372c4dba19bbdb0be07e43
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-03-30 13:55:52 -0500

    Create bootstrap scripts for OSX (#17)
    Adds scripts to install Go using `brew` and configure the user's shell
    by modifying `~/.bash_profile`.

---
+++ scripts/bootstrap-osx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+# Install Brew.
+brew --version || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Update Brew.
+brew update
+
+# Install brew formulas.
+brew install go
